b9edae3093b380fdee99183ece380fa8e566adff
umamumam <miftahulumam862@gmail.com>
2026-04-21 23:33:20 +0700

    feat: implement user registration controller and model with mass assignment attributes

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,8 @@ use Illuminate\Notifications\Notifiable;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable implements MustVerifyEmail
+// class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
